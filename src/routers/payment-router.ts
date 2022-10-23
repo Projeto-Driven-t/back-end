da27d233641b654dab5c21663707b4f92060e75d
GabsTiago <gabrielatiagodearaujo@outlook.com>
2022-10-23 15:14:49 -0300

    database: Remodels the  ticket table

--- src/routers/payment-router.ts
+++ src/routers/payment-router.ts
@@ -4,6 +4,6 @@ import { Router } from 'express';
 
 const paymentRouter = Router();
 
-paymentRouter.all('/*', authenticateToken).get('/', getTickets);
+paymentRouter.get('/', getTickets);
 
 export { paymentRouter };
