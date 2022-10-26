12e2f12b42eb8d1c5e3dd873e5e67a3f14326858
GabsTiago <gabrielatiagodearaujo@outlook.com>
2022-10-26 18:51:46 -0300

    feat: Create ticket repository function

--- src/repositories/payment-repository/index.ts
+++ src/repositories/payment-repository/index.ts
@@ -1,5 +1,5 @@
 import { prisma } from '@/config';
-import { Payment } from '@prisma/client';
+import { Payment, Ticket } from '@prisma/client';
 
 async function findModalities() {
   return prisma.modality.findMany();
@@ -21,10 +21,18 @@ async function create(payment: CreatePaymentParams, userId: number) {
   });
 }
 
+async function createTicket(ticket: CreateTicketParams, userId: number) {
+  return prisma.ticket.create({
+    data: { ...ticket, userId: userId },
+  });
+}
+
 export type CreatePaymentParams = Omit<Payment, 'id' | 'userId' | 'createdAt' | 'updatedAt'>;
+export type CreateTicketParams = Omit<Ticket, 'id' | 'userId' | 'createdAt' | 'updatedAt'>;
 
 const paymentRepository = {
   create,
+  createTicket,
   findFirst,
   findModalities,
   findAccommodations,
