cef915a6d314833204c628e2d5abe7fe62241b3e
GabsTiago <gabrielatiagodearaujo@outlook.com>
2022-10-30 14:41:57 -0300

    fix: Fixes seed

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -18,30 +18,96 @@ async function main() {
 
   let modality = await prisma.modality.findMany();
 
-  if (!modality) {
-    await prisma.modality.create({
-      data: { modality: 'Online', price: 100 },
-    });
-
-    await prisma.modality.create({
-      data: { modality: 'Presencial', price: 350 },
-    });
+  if (modality.length === 0) {
+    modality = [
+      await prisma.modality.create({
+        data: {
+          id: 1,
+          modality: 'Presencial',
+          price: 350,
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+      }),
+      await prisma.modality.create({
+        data: {
+          id: 2,
+          modality: 'Online',
+          price: 100,
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+      }),
+    ];
   }
 
-  let accommodation = await prisma.accommodation.findFirst();
+  let accommodation = await prisma.accommodation.findMany();
 
-  if(!accommodation) {
-    await prisma.accommodation.create({
-      data: {accommodation: "Com Hotel", price: 500}
-    })
+  if (accommodation.length === 0) {
+    accommodation = [
+      await prisma.accommodation.create({
+        data: {
+          id: 1,
+          accommodation: 'Sem Hotel',
+          price: 0,
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+      }),
+      await prisma.accommodation.create({
+        data: {
+          id: 2,
+          accommodation: 'Com Hotel',
+          price: 500,
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+      }),
+    ];
+  }
 
-    await prisma.accommodation.create({
-      data: {accommodation: "Sem Hotel", price: 0}
-    })
+  let hotels = await prisma.hotel.findMany();
+
+  if (hotels.length === 0) {
+    hotels = [
+      await prisma.hotel.create({
+        data: {
+          id: 1,
+          name: 'Driven Resort',
+          roomTypes: 'Single e Double',
+          imageUrl: 'https://media-cdn.tripadvisor.com/media/photo-s/16/1a/ea/54/hotel-presidente-4s.jpg',
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+      }),
+      await prisma.hotel.create({
+        data: {
+          id: 2,
+          name: 'Driven Palace',
+          roomTypes: 'Single, Double e Triple',
+          imageUrl: 'https://www.ahstatic.com/photos/5451_ho_00_p_1024x768.jpg',
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+      }),
+      await prisma.hotel.create({
+        data: {
+          id: 3,
+          name: 'Driven World',
+          roomTypes: 'Single, Double e Triple',
+          imageUrl:
+            'https://imgcy.trivago.com/c_fill,d_dummy.jpeg,e_sharpen:60,f_auto,h_627,q_auto,w_1200/itemimages/96/95/96959_v6.jpeg',
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+      }),
+    ];
   }
 
   console.log({ event });
-  console.log({ modality });
+  console.log(modality);
+  console.log(accommodation);
+  console.log(hotels);
 }
 
 main()
